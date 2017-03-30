56e72edd9f17b98cb8d30667c38664e8a67f5bea
VadimSquorpikkor <VadimSerikov11@gmail.com>
2017-03-30 04:25:02 +0300

    Initial commit

---
+++ app/src/main/java/com/squorpikkor/app/labaratory/MainActivity.java
@@ -0,0 +1,40 @@
+package com.squorpikkor.app.labaratory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    Button button1, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.textView);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.button1:
+                        //
+                        break;
+                    case R.id.button2:
+                        //
+                        break;
+                }
+            }
+        };
+
+        button1.setOnClickListener(listener);
+        button2.setOnClickListener(listener);
+    }
+}
